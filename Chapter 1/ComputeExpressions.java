c7724a28a515553684916b49083c130f694da4c5
Emily-html <19195060852m@sina.cn>
2024-09-30 18:42:41 +0800

    Add files via upload
    Signed-off-by: Emily-html <19195060852m@sina.cn>

---
+++ Chapter 1/ComputeExpressions.java
@@ -0,0 +1,7 @@
+public class CompleteExpressions {
+    public static void main(String[] args) throws Exception {
+        double number1 = 9.5 * 4.5 - 2.5 * 3;
+        double number2 = 45.5 - 3.5;
+        System.out.println(number1 / number2);
+    }
+}
---
+++ Chapter 1/SummationOfASeries.java
@@ -0,0 +1,7 @@
+public class DisplayThreeMessages {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+    }
