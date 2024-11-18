7a649214c37c48d1a34287fe159872729274e374
Emily-html <19195060852m@sina.cn>
2024-11-18 09:22:29 +0800

    Add files via upload
    Signed-off-by: Emily-html <19195060852m@sina.cn>

---
+++ Week10/Ep3.java
@@ -0,0 +1,11 @@
+public class Ep3 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = (int) (Math.random() * 20);
+            }
+        }
+        System.out.println(matrix[0][0]);
+    }
+}
